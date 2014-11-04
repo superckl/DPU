91a9b512a5eece1257ba6ee5930d47192a0f6186
superckl <lars4647@umn.edu>
2014-11-04 08:34:36 -0600

    Adding possibility of excludifier in dungeon chests.

--- src/main/java/me/superckl/dpu/common/reference/ModItems.java
+++ src/main/java/me/superckl/dpu/common/reference/ModItems.java
@@ -3,6 +3,9 @@ package me.superckl.dpu.common.reference;
 import me.superckl.dpu.common.item.ItemExcludifier;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraftforge.common.ChestGenHooks;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -17,6 +20,7 @@ public class ModItems {
 		GameRegistry.registerItem(ModItems.excludifier, "excludifier");
 		OreDictionary.registerOre("glass", Blocks.glass);
 		GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.excludifier,  "xyx", "xzx", "xwx", 'x', "ingotIron", 'y', "glass", 'z', Items.ender_pearl, 'w', Items.redstone));
+		ChestGenHooks.addItem(ChestGenHooks.DUNGEON_CHEST, new WeightedRandomChestContent(new ItemStack(ModItems.excludifier), 1, 1, 1));
 	}
 
 }
